9dfc160eb84dbbc77667afb443543d76236fd375
twonumberfortyfives <sma4no.7@gmail.com>
2024-05-09 23:50:57 +0300

    Solution (I've created template_creation to fix flake errors)

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import os
 import django
 
+
+def initialize_django():
+    # Initialize Django
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "car_service.settings")
+    django.setup()
+
+
 # Initialize Django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "car_service.settings")
-django.setup()
-
-from car.models import Car
-from car.serializers import CarSerializer
+initialize_django()
 
 
-def serialize_car_object(car: Car) -> dict:
+def serialize_car_object(car):
+    from car.serializers import CarSerializer
+
     # Serialize the car object
     car_serialized = CarSerializer(car)
     return car_serialized.data
 
 
-def deserialize_car_object(data: dict) -> Car:
+def deserialize_car_object(data):
+    from car.serializers import CarSerializer
+
     # Deserialize the car data
     serialized_car = CarSerializer(data=data)
     if serialized_car.is_valid():
@@ -26,27 +33,33 @@ def deserialize_car_object(data: dict) -> Car:
         return None
 
 
-# Create a car object
-car = Car.objects.create(
-    manufacturer="German",
-    model="BMW",
-    horse_powers=240,
-    is_broken=False,
-    problem_description=""
-)
+def template_creation():
+    from car.models import Car
 
-data = {
-    "manufacturer": "Sweden",
-    "model": "Volvo",
-    "horse_powers": 24,
-    "is_broken": False,
-    "problem_description": "motor"
-}
+    car = Car.objects.create(
+        manufacturer="German",
+        model="BMW",
+        horse_powers=240,
+        is_broken=False,
+        problem_description=""
+    )
+
+    data = {
+        "manufacturer": "Sweden",
+        "model": "Volvo",
+        "horse_powers": 24,
+        "is_broken": False,
+        "problem_description": "motor"
+    }
+
+    # Serialize the car object
+    serialized_object = serialize_car_object(car)
+    print("Serialized Car Data:", serialized_object)
+
+    # Deserialize the car data
+    deserialized_object = deserialize_car_object(data)
+    print("Deserialized Car Data:", deserialized_object)
 
 
-# Serialize the car object
-serialize_object = serialize_car_object(car)
-print("Serialized Car Data:", serialize_object)
-
-non_serialized_object = deserialize_car_object(data)
-print("Deserialized Car Data:", non_serialized_object)
+# Call the template_creation function
+template_creation()
