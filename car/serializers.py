da64a492b61fae8913ea949c47cffa4cd2d74dc1
twonumberfortyfives <sma4no.7@gmail.com>
2024-05-09 23:29:34 +0300

    Solution

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,30 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from rest_framework import serializers
+
+from car.models import Car
 
 
 class CarSerializer(serializers.Serializer):
-    pass
+    manufacturer = serializers.CharField(max_length=255)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(1914)]
+    )
+    is_broken = serializers.BooleanField()
+    problem_description = serializers.CharField(allow_blank=True, allow_null=True)
+
+    class Meta:
+        model = Car
+        fields = "__all__"
+
+    def create(self, validated_data):
+        return Car.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.manufacturer = validated_data.get("manufacturer", instance.manufacturer)
+        instance.model = validated_data.get("model", instance.model)
+        instance.horsepower = validated_data.get("horsepower", instance.horsepower)
+        instance.is_broken = validated_data.get("is_broken", instance.is_broken)
+        instance.problem_description = validated_data.get("problem_description", instance.problem_description)
+        instance.save()
+        return instance
--- main.py
+++ main.py
@@ -1,9 +1,52 @@
+import os
+import django
+
+# Initialize Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "car_service.settings")
+django.setup()
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
-def serialize_car_object(car: Car) -> bytes:
-    pass
+def serialize_car_object(car: Car) -> dict:
+    # Serialize the car object
+    car_serialized = CarSerializer(car)
+    return car_serialized.data
 
 
-def deserialize_car_object(json: bytes) -> Car:
-    pass
+def deserialize_car_object(data: dict) -> Car:
+    # Deserialize the car data
+    serialized_car = CarSerializer(data=data)
+    if serialized_car.is_valid():
+        return serialized_car.save()
+    else:
+        # Handle validation errors
+        print("Deserialization failed:", serialized_car.errors)
+        return None
+
+
+# Create a car object
+car = Car.objects.create(
+    manufacturer="German",
+    model="BMW",
+    horse_powers=240,
+    is_broken=False,
+    problem_description=""
+)
+
+data = {
+    "manufacturer": "Sweden",
+    "model": "Volvo",
+    "horse_powers": 24,
+    "is_broken": False,
+    "problem_description": "motor"
+}
+
+
+# Serialize the car object
+serialize_object = serialize_car_object(car)
+print("Serialized Car Data:", serialize_object)
+
+non_serialized_object = deserialize_car_object(data)
+print("Deserialized Car Data:", non_serialized_object)
